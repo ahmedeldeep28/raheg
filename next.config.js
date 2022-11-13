380e0e99449ce36b381f8b642393382df9ffd287
ahmed eldeep <a7medeldeep28@gmail.com>
2022-11-13 12:46:18 +0200

    production build 3

--- next.config.js
+++ next.config.js
@@ -1,9 +1,14 @@
 /** @type {import('next').NextConfig} */
+const path = require('path')
+
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
   images: {
     domains: ["raheg-api.vercel.app","localhost"],
+  },
+  sassOptions: {
+    includePaths: [path.join(__dirname, 'styles')],
   },
 }
 
